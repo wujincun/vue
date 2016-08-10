d0af52461991a315f4dd5af33849959b080640cb
wujincun <1769453779@qq.com>
2016-08-10 11:33:43 +0800

    注释

--- my-vue-project/src/main.js
+++ my-vue-project/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App'//同级目录下的App.vue
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: { App }//注册组件，就可以在index.html里直接使用<app></app>
 })
