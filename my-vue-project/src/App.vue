30cca897af09ce683c5a097b9f9c5012aa594e4e
wujincun <1769453779@qq.com>
2016-08-11 18:00:38 +0800

    newItem不随input里的内容而变化

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,64 +1,78 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+    <div id="app">
+        <!--<h1>{{title}}</h1>-->
+        <h1 v-text="title"></h1>
+        <input v-module="newItem" v-on:keyup.enter="addNew">
+        <ul>
+            <li v-for="item in items" :class="[liClass,{finished:item.isFinished}]" @click="toggleFinish(item)">
+                {{item.label}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
-export default {
-  components: {
-    Hello
-  }
-}
+
+    export default {
+        data () {
+            return {
+                title: 'this is a todo list!',
+                items: [
+                    {
+                        label: 'coding',
+                        isFinished: false
+                    },
+                    {
+                        label: 'walking',
+                        isFinished: true
+                    }
+                ],
+                liClass:'thisisliClass',
+                newItem:''
+            }
+        },
+        methods:{
+            toggleFinish:function (item) {
+                item.isFinished = !item.isFinished;
+            },
+            addNew:function () {
+                console.log(this.newItem)
+            }
+        }
+    }
 </script>
 
 <style>
-html {
-  height: 100%;
-}
+    .finished{
+        text-decoration: underline;
+    }
+    html {
+        height: 100%;
+    }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
+    #app {
+        color: #2c3e50;
+        margin-top: -100px;
+        max-width: 600px;
+        font-family: Source Sans Pro, Helvetica, sans-serif;
+        text-align: center;
+    }
 
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
+    #app a {
+        color: #42b983;
+        text-decoration: none;
+    }
 
-.logo {
-  width: 100px;
-  height: 100px
-}
+    .logo {
+        width: 100px;
+        height: 100px
+    }
 </style>
